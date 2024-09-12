008ca88642610f7c84e4e91b98a7890480470ee5
ci2bx <74463191+ci2bx@users.noreply.github.com>
2024-09-12 08:10:59 +0200

    Update script.js

--- apropo/script.js
+++ apropo/script.js
@@ -1,6 +1,6 @@
 function rick(){
 
-  document.getElementById("body").innerHTML = "<video autoplay><source src='/rr.mp4' type='video/mp4'></video>"
+  document.getElementById("body").innerHTML = "<video autoplay width=100%><source src='/rr.mp4' type='video/mp4'></video>"
   if (!document.fullscreenElement) {
   document.documentElement.requestFullscreen();
 }
